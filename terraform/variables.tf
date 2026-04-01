5bba43bac8e882371f373c8bc21748a984c8031f
ropant <a.ropers05@gmail.com>
2026-04-01 17:11:44 +0200

    Update variables.tf
    correction variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -2,20 +2,12 @@ variable "render_api_key" {
   type      = string
   sensitive = true
 }
-
 variable "render_owner_id" {
   type = string
 }
-
 variable "image_url" {
   type = string
 }
-
 variable "image_tag" {
   type = string
-}
-env_vars = {
-  ENV = {
-    value = "production"
-  }
 }
