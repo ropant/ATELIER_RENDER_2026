127fd3669d2d65fbc9b11bae9c9118d59b229f8d
ropant <a.ropers05@gmail.com>
2026-04-01 16:41:26 +0200

    Update main.tf
    ajout adminer dans le terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -21,12 +21,21 @@ resource "render_web_service" "flask_app" {
   name   = "flask-render-iac-${var.github_actor}"
   plan   = "free"
   region = "frankfurt"
-
   runtime_source = {
     image = {
       image_url = var.image_url
       tag       = var.image_tag
     }
   }
+}
 
+resource "render_web_service" "adminer" {
+  name   = "adminer-${var.github_actor}"
+  plan   = "free"
+  region = "frankfurt"
+  runtime_source = {
+    image = {
+      image_url = "docker.io/adminer"
+    }
+  }
 }
